889aadc833b0317f601d18a607dd2ad7dc77fdbd
Buzlu <buzlu2667@gmail.com>
2026-08-27 08:50:16 +0300

    Update Falizo web build with Admin Add Credits feature

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from 'next';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = 'https://maximorashop.com';
-  
+
   // Sabit sayfalar
   const staticEntries: MetadataRoute.Sitemap = [
     {
